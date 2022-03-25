b384990f35b662700a0eecaad8a23aeb2fc51f4e
Thomas Esseul <thomas@follow.fr>
2022-03-25 21:11:06 +0100

    chore(scripts): better command script

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -4,23 +4,31 @@ import { Routes } from 'discord-api-types/v9'
 import { clientId, guildId, token } from './config.json'
 import { help, prune } from './commands'
 
+const arg2 = process.argv[3]
+const globalDeploy = arg2.split('=').at(1) === 'true'
+
 const commands = [
 	new SlashCommandBuilder()
 		.setName(prune.name)
 		.setDescription(prune.description)
 		.addIntegerOption((option) =>
 			option.setName('amount').setDescription(prune.options.amount),
-	),
-	new SlashCommandBuilder().setName(help.name).setDescription(help.description).addStringOption(option => option.setName('command').setDescription(help.options.command))
+		),
+	new SlashCommandBuilder()
+		.setName(help.name)
+		.setDescription(help.description)
+		.addStringOption((option) =>
+			option.setName('command').setDescription(help.options.command),
+		),
 ].map((cmd) => cmd.toJSON())
 
 const rest = new REST({ version: '9' }).setToken(token)
 
+const route = globalDeploy
+	? Routes.applicationCommands(clientId)
+	: Routes.applicationGuildCommands(clientId, guildId)
+
 rest
-	.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.put(route, { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error)
-
-// Pour activer les commandes globalement (Met jusqu'à 1h d'update, pas recommandé en dev)
-// https://discordjs.guide/interactions/slash-commands.html#guild-commands
-// .put(Routes.applicationCommands(clientId), { body: commands })
