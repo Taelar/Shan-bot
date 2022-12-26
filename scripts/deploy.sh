b600b9b376b14fbe10417d3efa9f4ab8991527a5
Thomas Esseul <thomas@follow.fr>
2022-12-26 19:40:40 +0100

    chore(deploy): update deploy stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,4 @@
 echo Start deploy
-ssh $SSH_HOST -p $SSH_PORT 'pm2 delete Shanbot'
 ssh -A $SSH_HOST -p $SSH_PORT 'cd Shan-bot && git pull --rebase '
-ssh $SSH_HOST -p $SSH_PORT 'cd Shan-bot && npm ci && npm run build'
-ssh $SSH_HOST -p $SSH_PORT 'cd Shan-bot && node -v'
-# ssh $SSH_HOST -p $SSH_PORT 'cd Shan-bot && pm2 start --name Shanbot npm -- run start'
+ssh $SSH_HOST -p $SSH_PORT "cd Shan-bot && npm run docker:build && npm run docker:start"
 echo End deploy
