9b752362b3d04e7c612738c7be26c1ad1809bd33
Thomas Esseul <thomas@follow.fr>
2022-12-26 20:17:04 +0100

    chore(docker): make docker stop non blocking

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 echo Start deploy
 ssh -A $SSH_HOST -p $SSH_PORT 'cd Shan-bot && git pull --rebase '
+ssh $SSH_HOST -p $SSH_PORT "docker stop shanbot"
 ssh $SSH_HOST -p $SSH_PORT "cd Shan-bot && npm run docker:build && npm run docker:start"
 echo End deploy
