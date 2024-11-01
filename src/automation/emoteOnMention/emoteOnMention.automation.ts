16fe00c0af21ab25fc248083528774bdc7e81079
Thomas Esseul <esseul.tho@gmail.com>
2024-11-01 18:32:30 +0100

    feat(onMention): change target emote

--- src/automation/emoteOnMention/emoteOnMention.automation.ts
+++ src/automation/emoteOnMention/emoteOnMention.automation.ts
@@ -2,13 +2,14 @@ import { Automation } from '../../model/Automation'
 import { findEmoji } from '../../utils/emojis.utils'
 
 const BOTNAME = "Shan'bot"
+const EMOJI_NAME = 'paswar'
 
 export const emoteOnMention: Automation = (message, clientUser, state) => {
 	if (
 		message.content.toLowerCase().includes(BOTNAME.toLowerCase()) ||
 		message.mentions.has(clientUser)
 	) {
-		const emoji = findEmoji('duh', message)
+		const emoji = findEmoji(EMOJI_NAME, message)
 		if (emoji) {
 			message.react(emoji)
 		}
